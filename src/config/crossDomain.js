efe288c12c2ebc5e6436d9998bc07dd0abf56d75
TikGuan <Tik.Guan@walmart.com>
2022-04-24 11:42:42 +0800

    开启Access-Control-Allow-Headers字段

--- src/config/crossDomain.js
+++ src/config/crossDomain.js
@@ -1,7 +1,7 @@
 async function crossDomain(ctx, next) {
   ctx.set('Access-Control-Allow-Origin', '*');
   ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-  // ctx.set('Access-Control-Allow-Headers', 'Content-type, Content-Length, Authorization, Accept, X-Requesteed-With, x-token, sessionToken, token');
+  ctx.set('Access-Control-Allow-Headers', 'Content-type, Content-Length, Authorization, Accept, X-Requesteed-With, x-token, sessionToken, token');
 
   ctx.method === 'OPTIONS' ? (ctx.body = 200) : await next();
 }
